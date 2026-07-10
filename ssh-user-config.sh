7bb69a792dcd97adffd641e0efb2d21f889d55e2
naserkhanhejaazi <huggingfacematin@atomicmail.io>
2026-07-10 17:41:35 +0330

    Update default SSH username and password

--- ssh-user-config.sh
+++ ssh-user-config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set SSH_USERNAME and SSH_PASSWORD by default or create an .env file (refer to.env.example)
-: ${SSH_USERNAME:="myuser"}
-: ${SSH_PASSWORD:="mypassword"}
+: ${SSH_USERNAME:="Matin"}
+: ${SSH_PASSWORD:="Mn09189496020"}
 
 # Set root password if root login is enabled
 : ${ROOT_PASSWORD:=""}
